779f17f8bf682c9d124236e2f8dd21fc42c05f13
sreehari-cyberonics <sreehari@cyberonic.in>
2023-09-11 18:55:09 +0530

    core: api integration in hotelfields

--- src/jsx/components/Settings/Hotels/Fields/HotelFields.js
+++ src/jsx/components/Settings/Hotels/Fields/HotelFields.js
@@ -1,62 +1,54 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import FieldComponent from '../../../common/FieldComponent'
-// menu data
-const Data  = [
-  { id: 1, name: 'Field 1' },
-  { id: 2, name: 'Field 2' },
-  { id: 3, name: 'Field 3' },
-  { id: 4, name: 'Field 4' },
-  { id: 5, name: 'Field 5' },
-]
-
+import { URLS } from '../../../../../constants'
 
 export const PropertyCategory = () => {
   return (
     <>
-       <FieldComponent title='Property Category' addTitle='Category' tableData={Data}/>
+       <FieldComponent title='Property Category' addTitle='Category' url={URLS.PROPERTY_CATEGORY_URL}/>
     </>
   )
 }
 export const PropertyTypes = () => {
   return (
     <>
-       <FieldComponent title='Property Types' addTitle='Type' tableData={Data}/>
+       <FieldComponent title='Property Types' addTitle='Type' url={URLS.PROPERTY_TYPE_URL}/>
     </>
   )
 }
 export const RoomTypes = () => {
   return (
     <>
-       <FieldComponent title='Room Types' addTitle='Type' tableData={Data}/>
+       <FieldComponent title='Room Types' addTitle='Type' url={URLS.ROOM_TYPE_URL}/>
     </>
   )
 }
 export const MarketTypes = () => {
   return (
     <>
-       <FieldComponent title='Market Types' addTitle='Type' tableData={Data}/>
+       <FieldComponent title='Market Types' addTitle='Type' url={URLS.MARKET_TYPE_URL}/>
     </>
   )
 }
 export const RoomAmenities = () => {
   return (
     <>
-       <FieldComponent title='Room Amenities' addTitle='Amenity' tableData={Data}/>
+       <FieldComponent title='Room Amenities' addTitle='Amenity' url={URLS.ROOM_AMENITIES_URL}/>
     </>
   )
 }
 export const HotelAmenities = () => {
   return (
     <>
-       <FieldComponent title='Hotel Amenities' addTitle='Amenity' tableData={Data}/>
+       <FieldComponent title='Hotel Amenities' addTitle='Amenity' url={URLS.HOTEL_AMENITIES_URL}/>
     </>
   )
 }
 export const MealPlan = () => {
   return (
     <>
-       <FieldComponent title='Meal Plan' addTitle='Plan' tableData={Data}/>
+       <FieldComponent title='Meal Plan' addTitle='Plan' url={URLS.MEAL_PLAN_URL}/>
     </>
   )
 }
